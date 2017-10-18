2e3c0662b94f4317fe618238120c7722ac05c037
Bethany Shao <bethany.magdalena@gmail.com>
2017-10-18 19:51:04 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -5,7 +5,8 @@ def third_challenge
         #your key/value pairs here
         },
       :matriarch => {
-        #your key/value pairs here
+        :name => "Lord Montague",
+        :age =? "53"
         },
       :hero => {
         #your key/value pairs here
